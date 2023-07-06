df5e91cae54f09336ff2f97c5f9b5dac9eae82e6
ItsAiz <andres.sierra03@uptc.edu.co>
2023-07-05 23:02:21 -0500

    templates for dashboard and login added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
@@ -11,5 +12,7 @@ app.set('PORT', process.env.PORT || 4000)
 app.listen(app.get('PORT'), () =>{
     console.log(`Server deployed on port ${app.get('PORT')}`)
 })
+
+console.log("eooo"+ path.join(__dirname,'views'))
 
 app.use('/', require('./routes/web'))
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const router = express.Router()
+
+router.get("/", (req, res) =>{
+    res.render('login')
+})
+
+router.get("/admin", (req, res) =>{
+    res.render('templates/dashBoard_template')
+})
 
 module.exports = router
