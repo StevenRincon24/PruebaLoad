90df0b64fcab7b198b7520a2a6c077036616974f
StevenRincon24 <74875614+StevenRincon24@users.noreply.github.com>
2023-07-07 23:03:48 -0500

    Add book with persistence

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,38 +1,50 @@
-const express = require('express')
-const path = require('path')
-const service = require('../services/loginService')
+const express = require("express");
+const path = require("path");
+const service = require("../services/loginService");
+const serviceBook = require("../services/createBookService");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res) =>{
-    res.render('login')
-})
+router.get("/", (req, res) => {
+  res.render("login");
+});
 
-router.get("/admin", (req, res) =>{
-    res.render('templates/dashBoard_template')
-})
+router.get("/admin", (req, res) => {
+  res.render("templates/dashBoard_template");
+});
 
-router.post("/login/validateUser", service.validateLogin)
+router.post("/login/validateUser", service.validateLogin);
 
-router.get("/dashBoard", (req, res) =>{
-    req.session = req.session || {}
-    const rol = req.session.data
-    if (rol) {
-        res.render('./templates/dashBoard_template', { data: rol });
-    } else {
-        res.redirect('/')
-    }
-})
+router.get("/dashBoard", (req, res) => {
+  req.session = req.session || {};
+  const rol = req.session.data;
+  if (rol) {
+    res.render("./templates/dashBoard_template", { data: rol });
+  } else {
+    res.redirect("/");
+  }
+});
 
-router.get("/dashBoard/employeesManagement", (req, res) =>{
-    req.session = req.session || {}
-    const rol = req.session.data
-    if (rol) {
-        res.render('./admin/employeeManagement', { data: rol });
-    } else {
-        res.redirect('/')
-    }
-})
+router.get("/dashBoard/employeesManagement", (req, res) => {
+  req.session = req.session || {};
+  const rol = req.session.data;
+  if (rol) {
+    res.render("./admin/employeeManagement", { data: rol });
+  } else {
+    res.redirect("/");
+  }
+});
 
+router.get("/dashBoard/bookManagement", (req, res) => {
+  req.session = req.session || {};
+  const rol = req.session.data;
+  if (rol) {
+    res.render("./admin/bookManagement", { data: rol });
+  } else {
+    res.redirect("/");
+  }
+});
 
-module.exports = router
+router.post("/book/createBook", serviceBook.createBook);
+
+module.exports = router;
