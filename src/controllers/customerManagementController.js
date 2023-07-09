0663baa1c060247cd6a9deb7a96c9af96a9d329a
StevenRincon24 <74875614+StevenRincon24@users.noreply.github.com>
2023-07-08 21:54:20 -0500

    Review Loan

--- src/controllers/customerManagementController.js
+++ src/controllers/customerManagementController.js
@@ -1,4 +1,5 @@
-const data = require("../data/users.json");
+const data = require('../data/users.json')
+const dataBook = require("../data/book.json");
 const fs = require("fs");
 const path = require("path");
 const filePath = path.join(__dirname, "../data/users.json");
@@ -14,19 +15,55 @@ const getCustomerData = () => {
   return customers;
 };
 
-const registerCustomer = (
-  name,
-  lastName,
-  documentType,
-  documentNumber,
-  birthday,
-  cellphone,
-  address,
-  username,
-  password
-) => {
-  const rol = "customer";
-  const newUser = username;
+const registerCustomer = (name, lastName, documentType, documentNumber, birthday, cellphone, address, username, password) => {
+    const rol = "customer"
+    const newUser = username
+    const loans = []
+    const newUserData = {
+        password,
+        rol,
+        name,
+        lastName,
+        documentType,
+        documentNumber,
+        cellphone,
+        address,
+        birthday,
+        loans
+    }
+    return new Promise((resolve, reject) => {
+        data.usuarios[newUser] = newUserData;
+        const newContent = JSON.stringify(data, null, 2);
+        fs.writeFile(filePath, newContent, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(newUserData);
+            }
+        })
+    })
+}
+
+const deleteCustomer = (username) => {
+    return new Promise((resolve, reject) => {
+      if (data.usuarios.hasOwnProperty(username)) {
+        delete data.usuarios[username];
+        const newContent = JSON.stringify(data, null, 2)
+        fs.writeFile(filePath, newContent, (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        })
+      } else {
+        reject(new Error('El usuario no existe'));
+      }
+    })
+}
+
+
+const updateCustomer = (name, lastName, documentType, documentNumber, birthday, cellphone, address, username, password, rol) =>{
   const newUserData = {
     password,
     rol,
@@ -51,62 +88,52 @@ const registerCustomer = (
   });
 };
 
-const deleteCustomer = (username) => {
+
+
+const registerLoan = (username, ISBN) => {
   return new Promise((resolve, reject) => {
-    if (data.usuarios.hasOwnProperty(username)) {
-      delete data.usuarios[username];
-      const newContent = JSON.stringify(data, null, 2);
-      fs.writeFile(filePath, newContent, (err) => {
+    const currentDate = new Date();
+    const endDate = new Date(currentDate);
+    endDate.setDate(currentDate.getDate() + 8);
+    const startDateString = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`;
+    const endDateString = `${endDate.getFullYear()}-${(endDate.getMonth() + 1).toString().padStart(2, '0')}-${endDate.getDate().toString().padStart(2, '0')}`;
+
+    if (dataBook[ISBN].copies > 0) {
+      dataBook[ISBN].copies--;
+
+      data.usuarios[username].loans.push({
+        isbn: ISBN,
+        startDate: startDateString,
+        endDate: endDateString,
+        state: true
+      });
+
+      const newContentUsers = JSON.stringify(data, null, 2);
+      const newContentBooks = JSON.stringify(dataBook, null, 2);
+
+      fs.writeFile(filePath, newContentUsers, (err) => {
         if (err) {
           reject(err);
         } else {
-          resolve();
+          fs.writeFile(filePath2, newContentBooks, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
         }
       });
     } else {
-      reject(new Error("El usuario no existe"));
+      reject("No hay copias disponibles de este libro");
     }
   });
-};
+}
 
-const updateCustomer = (
-  name,
-  lastName,
-  documentType,
-  documentNumber,
-  birthday,
-  cellphone,
-  address,
-  username,
-  password
-) => {
-  const newUserData = {
-    password,
-    rol,
-    name,
-    lastName,
-    documentType,
-    documentNumber,
-    cellphone,
-    address,
-    birthday,
-  };
-  return new Promise((resolve, reject) => {
-    data.usuarios[username] = newUserData;
-    const newContent = JSON.stringify(data, null, 2);
-    fs.writeFile(filePath, newContent, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(newUserData);
-      }
-    });
-  });
-};
-
-module.exports = {
-  getCustomerData,
-  registerCustomer,
-  deleteCustomer,
-  updateCustomer,
-};
+module.exports ={
+    getCustomerData,
+    registerCustomer,
+    deleteCustomer,
+    updateCustomer,
+    registerLoan
+}
